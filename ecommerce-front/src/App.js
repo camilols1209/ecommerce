1d4a2bc62ac77821a78bf0c9512039ac342b957d
camilols1209 <jcsegre@uninorte.edu.co>
2022-01-18 00:26:22 -0500

    import Product

---
+++ ecommerce-front/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Poduct from './components/Product';
+function App() {
+  return (
+    <div className="App">
+      <Poduct/>
+    </div>
+  );
+}
+
+export default App;
